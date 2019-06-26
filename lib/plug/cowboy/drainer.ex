6ddd8aaff0ef85247366325a2725dd2f9b297800
Allen Madsen <Allen Madsen>
2019-06-25 20:10:51 -0400

    Address feedback and add more complete docs

--- lib/plug/cowboy/drainer.ex
+++ lib/plug/cowboy/drainer.ex
@@ -1,13 +1,37 @@
 defmodule Plug.Cowboy.Drainer do
   @moduledoc """
-  TODO: description
-  """
-  use GenServer
+  GenServer to drain cowboy connections at shutdown.
 
-  @doc """
-  TODO: description
+  When starting `Plug.Cowboy` in a supervision tree, it will create a listener that receives
+  requests and creates a connection process to handle that request. During shutdown, a
+  `Plug.Cowboy` process will immediately exit, closing the listener and any open connections
+  that are still being served. In most cases, it is desireable to allow connections to
+  complete before shutting down.
+
+  This module provides a GenServer that during shutdown will close listeners and wait
+  for connections to complete. It should be placed after other supervised processes that
+  handle cowboy connections.
+
+      defmodule Hello.Application do
+        use Application
+        def start(_type, _args) do
+          ...
+
+          children = [
+            {Plug.Cowboy, scheme: :http, plug: MyApp, options: [port: 4040]},
+            {Plug.Cowboy, scheme: :https, plug: MyApp, options: [port: 4041]},
+            {Plug.Cowboy.Drainer, refs: [MyApp.HTTP, MyApp.HTTPS]}
+          ]
+
+          opts = [strategy: :one_for_one, name: Hello.Supervisor]
+          Supervisor.start_link(children, opts)
+        end
+      end
 
   ## Options
+
+    * `:refs` - A list of refs to drain. `:all` is also supported and will drain all cowboy
+      listeners, including those started by means other than `Plug.Cowboy`.
 
     * `:id` - The ID for the process.
       Defaults to `Plug.Cowboy.Drainer`.
@@ -15,16 +39,17 @@ defmodule Plug.Cowboy.Drainer do
     * `:shutdown` - How long to wait for connections to drain.
       Defaults to 5000ms.
 
-    * `:refs` - The cowboy listener refs to drain connections for.
-      Defaults to all cowboy listeners.
-
     * `:drain_check_interval` - How frequently to check if a listener's
       connections have been drained.
       Defaults to 1000ms.
   """
+  use GenServer
+
+  @doc false
   @spec child_spec(opts :: Keyword.t()) :: Supervisor.child_spec()
   def child_spec(opts) when is_list(opts) do
     {spec_opts, opts} = Keyword.split(opts, [:id, :shutdown])
+    Keyword.fetch!(opts, :refs)
 
     Supervisor.child_spec(
       %{
@@ -56,7 +81,7 @@ defmodule Plug.Cowboy.Drainer do
     |> drain(Keyword.get(opts, :drain_check_interval, 1_000))
   end
 
-  defp drain(nil, drain_check_interval) do
+  defp drain(:all, drain_check_interval) do
     :ranch.info()
     |> Enum.map(&elem(&1, 0))
     |> drain(drain_check_interval)
