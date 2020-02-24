bceb2ed17e9a1f578ef24a5ba2c749d0cd9c6409
Tobias Pfeiffer <pragtob@gmail.com>
2020-02-24 17:42:04 +0100

    Fix mix format linting
    The last master build failed because of this:
    https://travis-ci.org/elixir-plug/plug_cowboy/jobs/643035548

--- lib/plug/cowboy.ex
+++ lib/plug/cowboy.ex
@@ -230,7 +230,7 @@ defmodule Plug.Cowboy do
     {timeout, opts} = Keyword.pop(opts, :timeout)
 
     if timeout do
-      Logger.warn "the :timeout option for Cowboy webserver has no effect and must be removed"
+      Logger.warn("the :timeout option for Cowboy webserver has no effect and must be removed")
     end
 
     opts = Keyword.delete(opts, :otp_app)
